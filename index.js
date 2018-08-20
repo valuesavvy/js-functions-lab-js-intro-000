883d2a83d8c976234c88309f1df86dbf20d5d295
James Powell <nvrquitjp@gmail.com>
2018-08-20 09:15:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 // write your code below!
 function happyHolidays(){
-  return('Happy holidays!')
+  return("Happy holidays!")
+}
+
+function happyHolidays(name){
+  name = James;
+  return('Happy holidays, ${name}!')
+}
+
+function happyHolidays(holiday, name){
+  holiday = Christmas;
+  name = Chreia;
+  return('Happy ${holiday}, ${name}!')
+}
+
+function holidayCountdown(days, holiday){
+  days = '23'
+  holiday = Christmas
+  return("It's ${days} days until ${holiday}!")
 }
